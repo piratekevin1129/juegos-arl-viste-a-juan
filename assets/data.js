e80e7beb75cafc4ac237780c0946c38173c5cade
Kevin Gaviria <kevingaviria.1994@gmail.com>
2020-08-11 03:12:33 -0500

    avances v4

--- assets/data.js
+++ assets/data.js
@@ -1,3 +1,182 @@
 var elementos = [
-	{}
+	{
+		id:1,
+		name:'Delantal',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:1,
+	},
+	{
+		id:2,
+		name:'Bata química',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:1,
+	},
+	{
+		id:3,
+		name:'Arnés',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:1,
+	},
+	{
+		id:4,
+		name:'Audífonos',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:2,
+	},
+	{
+		id:5,
+		name:'Botas pantaneras',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:3,
+	},
+	{
+		id:6,
+		name:'Botas de combate',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:3,
+	},
+	{
+		id:7,
+		name:'Botas de laboratorio',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:3,
+	},
+	{
+		id:8,
+		name:'Capucha',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:4,
+	},
+	{
+		id:9,
+		name:'Mascarilla',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:10,
+	},
+	{
+		id:10,
+		name:'Mascara de hierro',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:11,
+	},
+	{
+		id:11,
+		name:'Casco',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:5,
+	},
+	{
+		id:12,
+		name:'Casco con linterna',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:5,
+	},
+	{
+		id:13,
+		name:'Cuerda',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:6,
+	},
+	{
+		id:14,
+		name:'Máscara de buceo',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:7,
+	},
+	{
+		id:15,
+		name:'Gorra',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:5,
+	},
+	{
+		id:16,
+		name:'Gafas',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:7,
+	},
+	{
+		id:17,
+		name:'Guantes de combate',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:8,
+	},
+	{
+		id:18,
+		name:'Guantes de latex',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:8,
+	},
+	{
+		id:19,
+		name:'Guantes de nitrilo',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:8,
+	},
+	{
+		id:20,
+		name:'Guantes de tela',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:8,
+	},
+	{
+		id:21,
+		name:'Tapabocas',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:9,
+	},
+	{
+		id:22,
+		name:'Tapabocas tipo concha',
+		description:'Lorem ipsu dolor e tore adamo que nuestra e la cosi napoleta a bimar uj hastilape saldona abiamo',
+		parte:9,
+	}
+
 ]
+
+var oficios = [
+	{
+		id:1,
+		name:'Quimico',
+		description:'Este trabajo es de químico',
+		elementos:[
+			[2],[7],[16],[19],[22]
+		],
+	},
+	{
+		id:2,
+		name:'Alturas',
+		description:'Este trabajo es de alturas',
+		elementos:[
+			[3],[6],[11],[13],[17]
+		]
+	},
+	{
+		id:3,
+		name:'Alcantarilla',
+		description:'Este trabajo es de alcantarilla',
+		elementos:[
+			[3],[5],[12],[13],[18]
+		]
+	},
+	{
+		id:4,
+		name:'Soldador',
+		description:'Este trabajo es de soldador',
+		elementos:[
+			[1],[6],[8],[10],[20]
+		]
+	}
+]
+
+///KEYS FOR PARTES
+/*1 torzo
+2 orejas
+3 pies
+4 cara
+5 cabeza
+6 mano
+7 ojos
+8 manos
+9 boca
+10 encima de la cara*/
--- assets/js/game.js
+++ assets/js/game.js
@@ -6,10 +6,168 @@ function getRand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+var jobs_completed = []
+var actual_job = -1
+var lista_elementos = []
+
+function getJob(){
+	var job = getRand(0,(oficios.length-1))
+	var job_exists = jobs_completed.includes(job)
+	while(job_exists){
+		job = getRand(0,(oficios.length-1))
+		job_exists = jobs_completed.includes(job)
+	}
+	actual_job = job
+}
+
 function setGame(){
+	getJob()
+	console.log(actual_job)
 	setElementos()
+	loadElementos(0)
+	//putElementos()
+}
+
+function unorderArrayElementos(long){
+	var desorden = []
+	while(desorden.length<long){
+		var a = getRand(0,(long-1))
+		var a_exists = desorden.includes(a)
+		while(a_exists){
+			a = getRand(0,(long-1))
+			a_exists = desorden.includes(a)
+		}
+		desorden.push(a)
+	}
+
+	var nuevo = []
+	for(i = 0;i<long;i++){
+		nuevo.push(lista_elementos[desorden[i]])
+	}
+	lista_elementos = nuevo
 }
 
 function setElementos(){
+	var elementos_obligatorios = oficios[actual_job].elementos
+	for(i = 0;i<elementos_obligatorios.length;i++){
+		var indice = elementos_obligatorios[i]//esto es un array
+		for(j = 0;j<indice.length;j++){
+			lista_elementos.push(indice[j])
+		}
+	}
+
+	while(lista_elementos.length<20){
+		var elemento_random = getRand(0,(elementos.length-1))
+		var elemento_random_exists = lista_elementos.includes(elementos[elemento_random].id)
+		
+		while(elemento_random_exists){
+			elemento_random = getRand(0,(elementos.length-1))
+			elemento_random_exists = lista_elementos.includes(elementos[elemento_random].id)
+		}
+		lista_elementos.push(elementos[elemento_random].id)
+	}
+
 	
+	//desorganizar lista de elementos
+	unorderArrayElementos(lista_elementos.length)
+}
+
+function findElementIndex(id){
+	var ind = -1
+	for(var ii = 0;ii<elementos.length;ii++){
+		if(elementos[ii].id==id){
+			ind = ii
+		}
+	}
+	return ind
+}
+
+function loadElementos(e){
+	if(e==lista_elementos.length){
+		putElementos()
+	}else{
+		loadElemento(e)
+	}
+}
+
+function loadElemento(e){
+	var img = new Image()
+	img.onload = function(){
+		elementos[findElementIndex(lista_elementos[e])].width = this.width
+		elementos[findElementIndex(lista_elementos[e])].height = this.height
+		
+		img.onload = null
+		img.onerror = null
+		img = null
+		loadElementos((e+1))
+	}
+	img.onerror = function(){
+		console.log("Error loading element "+img.src)
+		img.onload = null
+		img.onerror = null
+		img = null
+		loadElementos((e+1))
+	}
+	img.src = 'assets/images/elementos/'+lista_elementos[e]+'.png'
+}
+
+function putElementos(){
+	for(i = 0;i<lista_elementos.length;i++){
+		var casillero_parent = getE('casillero'+(i+1))
+		var div_elemento = document.createElement('div')
+		div_elemento.className = 'elemento'
+		var indx = findElementIndex(lista_elementos[i])
+		
+		var div_elemento_img = document.createElement('div')
+		div_elemento_img.style.width = elementos[indx].width+'px'
+		div_elemento_img.style.height = elementos[indx].height+'px'
+		div_elemento_img.style.backgroundImage = 'url(assets/images/elementos/'+lista_elementos[i]+'.png)'
+
+		div_elemento.appendChild(div_elemento_img)
+		
+
+		var div_puerta = document.createElement('div')
+		div_puerta.className = 'locker-door locked-door-close'
+		div_puerta.setAttribute("onclick","clickPuerta(this,"+(i+1)+")")
+
+		casillero_parent.appendChild(div_elemento)
+		casillero_parent.appendChild(div_puerta)
+	}
+}
+
+var actual_puerta = null
+var actual_casillero = null
+var frente_actual = 20
+
+function clickPuerta(door,idlocker){
+	if(actual_casillero!=null){
+		//cerrar la que haya
+		actual_puerta.classList.remove('locked-door-open')
+		actual_puerta.classList.add('locked-door-close')
+		
+		//mirar si es el mismo
+		if(actual_casillero.id==String('casillero'+idlocker)){
+			//la misma. dejar asi
+			actual_puerta = null
+			actual_casillero = null
+		}else{
+			//es otra, abramos la nueva
+			actual_puerta = door
+			actual_casillero = getE('casillero'+idlocker)
+			actual_puerta.classList.remove('locked-door-close')
+			actual_puerta.classList.add('locked-door-open')
+			actual_casillero.style.zIndex = 100
+		}
+	}else{
+		//es primera vez, abramosla
+		actual_puerta = door
+		actual_casillero = getE('casillero'+idlocker)
+		actual_puerta.classList.remove('locked-door-close')
+		actual_puerta.classList.add('locked-door-open')
+		actual_casillero.style.zIndex = 1
+	}
+}
+
+function getE(idname){
+	return document.getElementById(idname)
 }
