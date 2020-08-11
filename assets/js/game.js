d2ff0760b19f53d5a7f2f0fcf6c8d6c5ed3b2aab
Kevin Gaviria <kevingaviria.1994@gmail.com>
2020-08-10 22:54:01 -0500

    avances 3

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+var i = 0
+var j = 0
+var k = 0
+
+function getRand(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function setGame(){
+	setElementos()
+}
+
+function setElementos(){
+	
+}
